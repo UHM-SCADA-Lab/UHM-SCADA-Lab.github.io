7c90bf5cf3e361c2afdfbfbe4417ffa9e4f2410d
jebryce <116392465+jebryce@users.noreply.github.com>
2023-05-01 14:25:55 -1000

    Reworded the Lab Configuration page

--- src/departments/SFlowSDN/SDN/LabConfiguration.jsx
+++ src/departments/SFlowSDN/SDN/LabConfiguration.jsx
@@ -8,7 +8,7 @@ const LabConfiguration = () => (
     <h3>Concise Changes</h3>
     <ul>
       <li>The SDN controller needs to push flows to the hardware flow table, Table 100.</li>
-      <li>The SDN controller doesn&apos;t need to build a MAC address table to know where to forward packets to, instead the controller just forwards packets to the switch&apos;s normal processing, as our HP 2920-24G switches still build their own MAC address tables.</li>
+      <li>The SDN controller will create flows that forward packets to the network switch&apos;s normal forwarding plane. This avoids the need of the SDN controller to create a MAC address table.</li>
     </ul>
     <h3>Flow Tables</h3>
     <p>By default (our current configuration), there are three flow tables within each HP 2920-24G network switch.</p>
@@ -17,14 +17,14 @@ const LabConfiguration = () => (
     <ul>
       <li>Contains a single, match-all flow that forwards packets to &quot;Table 100&quot;.</li>
       <li>The table that Table 0 forwards to is configurable within the switch&apos;s configuration.</li>
-      <li>The HP 2920-24G network switch does not allow other flows to be added to Table 0. (It took me a literal month to figure this one out...)</li>
+      <li>The HP 2920-24G network switch does not allow other flows to be added to Table 0, as such flows will be added to either Table 100 or Table 200. (It took me a literal month to figure this one out...)</li>
     </ul>
     <h5>Table 100</h5>
     <ul>
       <li>This is known as the &quot;hardware table&quot; as the switching within the table is handled in hardware.</li>
       <li>As such, network traffic is able to be switched at ping latency of less than 1 ms.</li>
       <li>However, the downside of using this flow table is that not all possible match criteria is able to be used in hardware. See the &quot;Supported Matching Criteria&quot; section below.</li>
-      <li>By default, contains no flows, as such, if any packet is forwarded to a table without any flows, the packet will be dropped</li>
+      <li>Contains no flows by default.</li>
       <li>This will be the flow table that our controller will add flows to.</li>
     </ul>
     <h5>Table 200</h5>
@@ -32,7 +32,7 @@ const LabConfiguration = () => (
       <li>This is known as the &quot;software table&quot; as the switching within the table is handled in software inside the switch.</li>
       <li>As such, network traffic is switched slower than the hardware table, at ping latency of around 125 ms.</li>
       <li>Although this speed is extremely slow, the upside of using the software table as there are more possible match criteria than in the hardware table. However, the vastly decreased speeds negate this benefit.</li>
-      <li>By default, contains no flows, as such, if any packet is forwarded to a table without any flows, the packet will be dropped.</li>
+      <li>Contains no flows by default.</li>
     </ul>
     <h5>Supported Matching Criteria<sup>1</sup></h5>
     <Table striped bordered responsive="xl">
@@ -90,24 +90,25 @@ const LabConfiguration = () => (
     <sup>3</sup> Found from <Link to="https://community.hpe.com/t5/software-defined-networking/using-hardware-flows-table-with-hp-2920-24g/td-p/6931454#.ZExGNi-B3BU">HP community post detailing acceptable match criteria in hardware</Link>. <br />
     <br />
     <h3>Forwarding Actions</h3>
-    <p>Initially, we copied a basic controller program, <Link to="https://github.com/faucetsdn/ryu/blob/master/ryu/app/simple_switch_13.py">simple_switch_13.py</Link>, provided by the RYU SDN framework. This base program completely takes over all the decision making in forwarding packets, including telling which port the switch needs to send a packet out of. However, this requires the controller to build a MAC address table of where every device on the network. That is, the controller needs to know which port each device is on to be able to know which port to forward a packet to that device. Luckily, the HP 2920-24G happens to build it&apos;s MAC address table regardless if OpenFlow is enabled or disabled, as such, we can use the OpenFlow port &quot;OFPP_NORMAL&quot; to forward packets to be processed by the switch&apos;s normal switching. This allows us to focus our controller on solely deciding whether or not to allow or deny flows. See the following &quot;Possible OpenFlow Ports&quot; table for explanations of other OpenFlow ports and whether or not they are supported by the HP 2920-24G switch.</p>
-    <h5>Possible OpenFlow Ports<sup>1</sup></h5>
+    <p>Our initial implementation of SDN was based off an example program, <Link to="https://github.com/faucetsdn/ryu/blob/master/ryu/app/simple_switch_13.py">simple_switch_13.py</Link>, provided by the RYU SDN framework. This program creates flows that forward packets straight to their physical destination port on the network switch. However, this requires the controller to build a MAC address table of where every device on the network. That is, the controller needs to know which port each device is connected to. Luckily, the HP 2920-24G network switch happens to build it&apos;s MAC address table regardless if OpenFlow is enabled or disabled. As such, we can use the OpenFlow port &quot;OFPP_NORMAL&quot; to forward packets to be processed by the switch&apos;s normal switching. </p>
+    <p>See the following &quot;Possible OpenFlow Ports&quot; table for explanations of OpenFlow ports, whether or not the port is a valid destination to forward packets to, and whether or not the port is supported by the HP 2920-24G switch.</p>
+    <h5>Possible OpenFlow Ports<sup>4</sup></h5>
     <Table striped bordered responsive="xl">
       <thead>
-        <tr> <th>OpenFlow Port Name </th> <th>Description                                                                          </th> <th>Supported by the Switch </th> </tr>
+        <tr> <th>OpenFlow Port Name </th> <th>Description                                               </th> <th>Used in Packet Forwarding?</th> <th>Supported by the Switch </th> </tr>
       </thead>
       <tbody>
-        <tr> <td>OFPP_IN_PORT       </td> <td>Forwards the packet out the input port.                                              </td> <td>Unknown                 </td> </tr>
-        <tr> <td>OFPP_TABLE         </td> <td>Not used in packet forwarding. Performs actions in the flow table.                   </td> <td>Unknown                 </td> </tr>
-        <tr> <td>OFPP_NORMAL        </td> <td>Forwards the packet to be processed by the switch&apos;s normal switching.           </td> <td>Yes                     </td> </tr>
-        <tr> <td>OFPP_FLOOD         </td> <td>Forwards the packet to all physical ports except input ports and blocked ports.      </td> <td>Yes                     </td> </tr>
-        <tr> <td>OFPP_ALL           </td> <td>Forwards the packet to all physical ports except input ports.                        </td> <td>Unknown                 </td> </tr>
-        <tr> <td>OFPP_CONTROLLER    </td> <td>Forwards the packet to the controller as a flow request.                             </td> <td>Yes                     </td> </tr>
-        <tr> <td>OFPP_LOCAL         </td> <td>Local OpenFlow &quot;port&quot;. Indicates a local port of the switch.               </td> <td>No                      </td> </tr>
-        <tr> <td>OFPP_ANY           </td> <td>Not used in packet forwarding. Used as a wildcard in certain messages to the switch. </td> <td>Yes                     </td> </tr>
+        <tr> <td>OFPP_IN_PORT       </td> <td>Forwards the packet out the input port.                                         </td> <td>Yes </td> <td>Unknown </td> </tr>
+        <tr> <td>OFPP_TABLE         </td> <td>Performs actions in the flow table.                                             </td> <td>No  </td> <td>Unknown </td> </tr>
+        <tr> <td>OFPP_NORMAL        </td> <td>Forwards the packet to be processed by the switch&apos;s normal switching.      </td> <td>Yes </td> <td>Yes     </td> </tr>
+        <tr> <td>OFPP_FLOOD         </td> <td>Forwards the packet to all physical ports except input ports and blocked ports. </td> <td>Yes </td> <td>Yes     </td> </tr>
+        <tr> <td>OFPP_ALL           </td> <td>Forwards the packet to all physical ports except input ports.                   </td> <td>Yes </td> <td>Unknown </td> </tr>
+        <tr> <td>OFPP_CONTROLLER    </td> <td>Forwards the packet to the controller as a flow request.                        </td> <td>Yes </td> <td>Yes     </td> </tr>
+        <tr> <td>OFPP_LOCAL         </td> <td>Local OpenFlow &quot;port&quot;. Indicates a local port of the switch.          </td> <td>Yes </td> <td>No      </td> </tr>
+        <tr> <td>OFPP_ANY           </td> <td>Used as a wildcard in certain messages to the switch.                           </td> <td>No  </td> <td>Yes     </td> </tr>
       </tbody>
     </Table>
-    <sup>1</sup> Table adapted from <Link to="https://github.com/openstack/os-ken/blob/dcd0d1a1eeb12fe7de64b3c3a7e1f8f64d86e37e/os_ken/ofproto/ofproto_v1_3.py">OS-Ken&apos;s source code</Link> and <Link to="https://osrg.github.io/ryu-book/en/html/openflow_protocol.html">RYU&apos;s documentation</Link>, whether or not the switch supports the OpenFlow port was found experimentally.<br />
+    <sup>4</sup> Table adapted from <Link to="https://github.com/openstack/os-ken/blob/dcd0d1a1eeb12fe7de64b3c3a7e1f8f64d86e37e/os_ken/ofproto/ofproto_v1_3.py">OS-Ken&apos;s source code</Link> and <Link to="https://osrg.github.io/ryu-book/en/html/openflow_protocol.html">RYU&apos;s documentation</Link>. The &quot;Supported by the Switch&quot; column was determined experimentally.<br />
   </Container>
 );
 
